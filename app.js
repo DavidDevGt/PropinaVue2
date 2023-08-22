2ff6045a4c9c9907fe5f3166383d8b7b200041a8
Davidvl <josuedavidvl18@gmail.com>
2023-08-22 15:04:17 -0600

    Quite la funcionalidad de conversor de moneda, reduje las monedas admitidas y mejore la validacion de errores

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 new Vue({
     el: '#app',
     data: {
-        billAmount: 0,
-        tipPercentage: 15,
+        billAmount: '',
+        tipPercentage: '',
         tipAmount: 0,
-        totalAmount: 0
+        totalAmount: 0,
+        selectedCurrency: 'USD',
+        currencySymbols: {
+            'USD': '$',
+            'GTQ': 'Q',
+            'MXN': '$',
+            'COP': '$'
+        },
+        error: ''
+    },
+    watch: {
+        // Observar cambios en la selección de moneda para limpiar los campos
+        selectedCurrency() {
+            this.billAmount = '';
+            this.tipPercentage = '';
+            this.tipAmount = 0;
+            this.totalAmount = 0;
+            this.error = '';
+        }
     },
     methods: {
         calculateTip() {
-            this.tipAmount = this.billAmount * (this.tipPercentage / 100);
-            this.totalAmount = this.billAmount + this.tipAmount;
+            // Validación básica
+            if (!this.billAmount || isNaN(this.billAmount) || this.billAmount < 0) {
+                this.error = "Por favor, ingrese un monto de factura válido.";
+                return;
+            }
+
+            if (!this.tipPercentage || isNaN(this.tipPercentage) || this.tipPercentage < 0 || this.tipPercentage > 100) {
+                this.error = "Por favor, ingrese un porcentaje de propina válido.";
+                return;
+            }
+
+            this.error = '';  // Limpiar mensajes de error anteriores
+
+            // Calculamos la propina basada en el monto ingresado
+            this.tipAmount = parseFloat(this.billAmount) * (parseFloat(this.tipPercentage) / 100);
+
+            // Calculamos el total sumando el monto y la propina
+            this.totalAmount = parseFloat(this.billAmount) + this.tipAmount;
         }
     }
 });
