8c527a4d5013ad15a5358785ca1928fa8b88a6ad
Davidvl <josuedavidvl18@gmail.com>
2023-08-22 14:03:50 -0600

    Logica con Vue terminada

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-new
+new Vue({
+    el: '#app',
+    data: {
+        billAmount: 0,
+        tipPercentage: 15,
+        tipAmount: 0,
+        totalAmount: 0
+    },
+    methods: {
+        calculateTip() {
+            this.tipAmount = this.billAmount * (this.tipPercentage / 100);
+            this.totalAmount = this.billAmount + this.tipAmount;
+        }
+    }
+});
